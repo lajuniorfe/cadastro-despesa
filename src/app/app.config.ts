577c016ada82c56de67aea198cea8abbd5f4752a
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2024-10-29 20:48:23 -0300

    build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,17 @@ import {
   ApplicationConfig,
   importProvidersFrom,
   provideZoneChangeDetection,
+  LOCALE_ID,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { PrimeNGConfig } from 'primeng/api';
+
+registerLocaleData(localePt); // Registre os dados de localidade antes de configurar os provedores.
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +20,18 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     importProvidersFrom(BrowserAnimationsModule),
     provideHttpClient(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    {
+      provide: PrimeNGConfig,
+      useFactory: () => {
+        const config = new PrimeNGConfig();
+        config.setTranslation({
+          accept: 'Accept',
+          reject: 'Cancel',
+          //translations
+        });
+        return config;
+      },
+    },
   ],
 };
