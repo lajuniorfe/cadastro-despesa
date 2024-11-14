e493ac993fe6d68cfa627893ba70ceeb96a75797
lajuniorfe <33765211+lajuniorfe@users.noreply.github.com>
2024-11-14 15:15:49 -0300

    Cria nova variavel de ambiente dev para consumo de api de dados

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5083/api', // URL do backend para desenvolvimento
-  // outras variáveis de ambiente
+  apiUrl: 'http://localhost:5083/api', // URL do backend c# para desenvolvimento
+  apiUrlDados: 'http://localhost:8000',
 };
